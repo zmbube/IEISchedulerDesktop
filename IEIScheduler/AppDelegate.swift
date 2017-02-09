f1b47891b92c5d984df16718b147b9e27b382db1
Zachary Bube <zmbube@bsu.edu>
2017-02-09 12:31:30 -0500

    Initial Commit

---
+++ IEIScheduler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IEIScheduler
+//
+//  Created by Zachary Bube on 2/9/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IEIScheduler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IEIScheduler
+//
+//  Created by Zachary Bube on 2/9/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
