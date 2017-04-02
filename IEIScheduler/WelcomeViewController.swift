8bd526f2956064bdb4d957830fafa585e99be826
Zachary Bube <zmbube@bsu.edu>
2017-04-02 18:32:17 -0400

    Adjust some views alignments and input types

---
+++ IEIScheduler/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  IEIScheduler
+//
+//  Created by Zachary Bube on 4/2/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class WelcomeViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
