43ad29aaf6ca11170b8327c0ccc1632d8689688b
Zachary Bube <zmbube@bsu.edu>
2017-04-03 11:51:43 -0400

    Add controllers for all views

---
+++ IEIScheduler/AdminController.swift
@@ -0,0 +1,18 @@
+//
+//  AdminController.swift
+//  IEIScheduler
+//
+//  Created by Zachary Bube on 4/3/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class AdminController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ IEIScheduler/PreferenceOverrideController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferenceOverrideController.swift
+//  IEIScheduler
+//
+//  Created by Zachary Bube on 4/3/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class PreferenceOverrideController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
