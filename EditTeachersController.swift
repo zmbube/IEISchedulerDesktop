9fc54a2f1428eaa189811fc08c18a2fa32bf7ace
Public Account <dtgoelz@bsu.edu@ul-edrmac1-001.local>
2017-04-06 15:13:19 -0400

    added view controllers to both edit teacher and login

---
+++ EditTeachersController.swift
@@ -0,0 +1,22 @@
+//
+//  editTeachersController.swift
+//  IEIScheduler
+//
+//  Created by dtgoelz on 4/6/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class EditTeachersController: NSViewController {
+
+    @IBOutlet weak var editTeacherTable: NSScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+   
+    @IBAction func deleteTeacherEditButton(_ sender: AnyObject) {
+    }
+    
+}
---
+++ IEIScheduler/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  IEIScheduler
+//
+//  Created by dtgoelz on 4/6/17.
+//  Copyright © 2017 CSCapstoneSection2. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    
+    @IBOutlet weak var userNameTextBox: NSTextField!
+    @IBOutlet weak var passwordTextBox: NSTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    @IBAction func loginButtton(_ sender: AnyObject) {
+    }
+    
+}
