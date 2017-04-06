afd163000aa33d813cbd340388db8b9ba0d9c233
Public Account <mrduffy@bsu.edu@ul-edrmac1-003.local>
2017-04-06 15:40:45 -0400

    add cells to RoomController and PreferenceOverrideController

--- IEIScheduler/PreferenceOverrideController.swift
+++ IEIScheduler/PreferenceOverrideController.swift
@@ -11,50 +11,48 @@ import Cocoa
 class PreferenceOverrideController: NSViewController {
     @IBOutlet weak var teacherTable: NSTableView!
     @IBOutlet weak var classTable: NSTableView!
-    let teacherInfoUrl="https://www.zmbube.com/API/selectTeacher.php"
+    let classes = ["101", "102", "103", "104", "105", "106", "111", "112", "113", "114", "115", "116", "121", "122", "123", "124", "125", "126", "131", "132", "133", "134", "135", "136", "141", "142", "143", "144", "145", "146", "151", "152", "153", "154", "155", "156", "151G", "152G", "153G", "154G", "155G", "156G"]
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
-        
-        
-        
+        classTable.delegate=self
+        classTable.dataSource=self
     }
 
     
     @IBAction func teach(_ sender: NSButton) {
         //override the class preference and set it to 0
-        var teachers=[Teacher]()
-        let teachersInfo=parseUrl(urlInput: teacherInfoUrl)
-        for teacher in teachersInfo {
-            let temp = Teacher(name: "\(teacher["F_Name"] as! String) \(teacher["L_Name"] as! String)" ,id:teacher["U_ID"] as! String)
-            teachers.append(temp)
-        }
-        let classPrefs=parseUrl(urlInput: classPrefUrl)
-        
-        for prefs in classPrefs {
-            for teacher in teachers {
-                if(teacher.id==(prefs["U_ID"] as! String)){
-                    teacher.classPreferences = 0
-                }
-            }
+        //var teacher=[Teacher]()
+        //teacher.classPreferences[] = 0
 
-        }
+    }
     @IBAction func dontTeach(_ sender: NSButton) {
-        //override the class preference and set it to 5var teachers=[Teacher]()
-        let teachersInfo=parseUrl(urlInput: teacherInfoUrl)
-        for teacher in teachersInfo {
-            let temp = Teacher(name: "\(teacher["F_Name"] as! String) \(teacher["L_Name"] as! String)" ,id:teacher["U_ID"] as! String)
-            teachers.append(temp)
-        }
-        let classPrefs=parseUrl(urlInput: classPrefUrl)
-        
-        for prefs in classPrefs {
-            for teacher in teachers {
-                if(teacher.id==(prefs["U_ID"] as! String)){
-                    teacher.classPreferences = 5
-                }
-            }
+        //override the class preference and set it to 5
+        //var teacher=[Teacher]()
+        //teacher.classPreferences[] = 5
+    }
 
-        }
 }
+extension  PreferenceOverrideController: NSTableViewDataSource{
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return classes.count
+    }
+}
+
+extension PreferenceOverrideController: NSTableViewDelegate{
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellIdentifier = "classCell"
+        
+        if tableView == self.classTable{
+            if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+                cell.textField?.stringValue = self.classes[row]
+                return cell
+            }
+        }
+        return nil
+    }}
