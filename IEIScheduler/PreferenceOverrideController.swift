d44274f0caa57f4e25144bb0c94f720e175a2f5f
Meghan Duffy <mrduffy@bsu.edu>
2017-04-06 13:54:07 -0400

    start work on making the pref override actually override prefs. this push is just so I can work on it on the library computers lol

--- IEIScheduler/PreferenceOverrideController.swift
+++ IEIScheduler/PreferenceOverrideController.swift
@@ -11,6 +11,7 @@ import Cocoa
 class PreferenceOverrideController: NSViewController {
     @IBOutlet weak var teacherTable: NSTableView!
     @IBOutlet weak var classTable: NSTableView!
+    let teacherInfoUrl="https://www.zmbube.com/API/selectTeacher.php"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +20,41 @@ class PreferenceOverrideController: NSViewController {
         
         
     }
+
     
-    @IBAction func teach(_ sender: AnyObject) {
-    }
-    @IBAction func dontTeach(_ sender: AnyObject) {
-    }
+    @IBAction func teach(_ sender: NSButton) {
+        //override the class preference and set it to 0
+        var teachers=[Teacher]()
+        let teachersInfo=parseUrl(urlInput: teacherInfoUrl)
+        for teacher in teachersInfo {
+            let temp = Teacher(name: "\(teacher["F_Name"] as! String) \(teacher["L_Name"] as! String)" ,id:teacher["U_ID"] as! String)
+            teachers.append(temp)
+        }
+        let classPrefs=parseUrl(urlInput: classPrefUrl)
+        
+        for prefs in classPrefs {
+            for teacher in teachers {
+                if(teacher.id==(prefs["U_ID"] as! String)){
+                    teacher.classPreferences = 0
+                }
+            }
+
+        }
+    @IBAction func dontTeach(_ sender: NSButton) {
+        //override the class preference and set it to 5var teachers=[Teacher]()
+        let teachersInfo=parseUrl(urlInput: teacherInfoUrl)
+        for teacher in teachersInfo {
+            let temp = Teacher(name: "\(teacher["F_Name"] as! String) \(teacher["L_Name"] as! String)" ,id:teacher["U_ID"] as! String)
+            teachers.append(temp)
+        }
+        let classPrefs=parseUrl(urlInput: classPrefUrl)
+        
+        for prefs in classPrefs {
+            for teacher in teachers {
+                if(teacher.id==(prefs["U_ID"] as! String)){
+                    teacher.classPreferences = 5
+                }
+            }
+
+        }
 }
