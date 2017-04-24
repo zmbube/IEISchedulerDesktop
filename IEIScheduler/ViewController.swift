293b8d1cba45b60b6e03250a3df3a46b9a8b6ef1
Zachary Bube <zmbube@bsu.edu>
2017-04-24 13:35:28 -0400

    Change teacher color in main view to align with preference

--- IEIScheduler/ViewController.swift
+++ IEIScheduler/ViewController.swift
@@ -77,12 +77,16 @@ extension ViewController: NSTableViewDelegate{
         var text2=""
         var text3=""
         var text4=""
+        var text2Color=NSColor.black
         
         if(tableColumn == classTable.tableColumns[0]){
             text1=(classSections[selectedLevel]?[row+1]?[0].classTitle)!
             text2=classSections[selectedLevel]?[row+1]?[0].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[0].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[0].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[0].teacher)! , className: text1)
+            }
             
         }
         else if(tableColumn!==classTable.tableColumns[1] && (classSections[selectedLevel]?[row+1]?.endIndex)! > 1){
@@ -90,36 +94,52 @@ extension ViewController: NSTableViewDelegate{
             text2=classSections[selectedLevel]?[row+1]?[1].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[1].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[1].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[1].teacher)! , className: text1)
+            }
         }
         else if(tableColumn!==classTable.tableColumns[2] && (classSections[selectedLevel]?[row+1]?.endIndex)! > 2){
             text1=(classSections[selectedLevel]?[row+1]?[2].classTitle)!
             text2=classSections[selectedLevel]?[row+1]?[2].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[2].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[2].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[2].teacher)! , className: text1)
+            }
         }
         else if(tableColumn!==classTable.tableColumns[3] && (classSections[selectedLevel]?[row+1]?.endIndex)! > 3){
             text1=(classSections[selectedLevel]?[row+1]?[3].classTitle)!
             text2=classSections[selectedLevel]?[row+1]?[3].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[3].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[3].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[3].teacher)! , className: text1)
+            }
         }
         else if(tableColumn!==classTable.tableColumns[4] && (classSections[selectedLevel]?[row+1]?.endIndex)! > 4){
             text1=(classSections[selectedLevel]?[row+1]?[4].classTitle)!
             text2=classSections[selectedLevel]?[row+1]?[4].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[4].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[4].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[4].teacher)! , className: text1)
+            }
         }
         else if(tableColumn!==classTable.tableColumns[5] && (classSections[selectedLevel]?[row+1]?.endIndex)! > 5){
             text1=(classSections[selectedLevel]?[row+1]?[5].classTitle)!
             text2=classSections[selectedLevel]?[row+1]?[5].teacher?.name ?? "NA"
             text3=classSections[selectedLevel]?[row+1]?[5].time ?? "NA"
             text4=classSections[selectedLevel]?[row+1]?[5].room?.buildingID ?? "NA"
+            if(text2 != "NA"){
+                text2Color=textColor(teacher: (classSections[selectedLevel]?[row+1]?[5].teacher)! , className: text1)
+            }
         }
         if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? TeacherTableCell {
             cell.textField?.stringValue = text1
             cell.textField2?.stringValue = text2
+            cell.textField2?.textColor=text2Color
             cell.textField3?.stringValue="Time: \(text3)"
-            cell.textField4.stringValue="Room: \(text4)"
+            cell.textField4?.stringValue="Room: \(text4)"
             return cell
         }
         
@@ -130,6 +150,24 @@ extension ViewController: NSTableViewDelegate{
         optionsTableView?.display="section"
         optionsTableView?.selectedLevel=selectedLevel
         optionsTableView?.reloadForSection(section: classTable.selectedRow+1)
+    }
+    
+    func textColor(teacher:Teacher, className:String)->NSColor{
+        if((teacher.classPreferences[className] as? String) == "1"){
+            return NSColor.green
+        }
+        else if((teacher.classPreferences[className] as? String) == "2"){
+            return NSColor.yellow
+        }
+        else if((teacher.classPreferences[className] as? String) == "3"){
+            return NSColor.black
+        }
+        else if((teacher.classPreferences[className] as? String) == "4"){
+            return NSColor.red
+        }
+        
+        
+        return NSColor.black
     }
     
 }
